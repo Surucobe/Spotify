95e84fa06d02f37a60ecae3c59fdb07378f7003a
Surucobe <garudahorror25@gmail.com>
2021-08-09 20:49:47 -0400

    tomorrow big backend day

--- Server/Server.js
+++ Server/Server.js
@@ -7,10 +7,8 @@ app.get('/', (req, res) =>{
    console.log('currently in the root')
 })
 
-app.get('/login', (req, res) =>{
+app.post('/login', (req, res) =>{
   console.log('Moving onto the login section')
 })
 
 console.log(`Listening to port: ` + `${port}`.red)
-
-app.listen(port);

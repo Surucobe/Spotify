5f2bbd41b61b451bb80146c685d07476c2211fad
Surucobe <garudahorror25@gmail.com>
2021-08-10 20:44:19 -0400

    added some padding to the navbar

--- Server/Server.js
+++ Server/Server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const colors = require('colors');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) =>{
    console.log('currently in the root')
 })
 
-app.post('/login', (req, res) =>{
+app.get('/login', (req, res) =>{
   console.log('Moving onto the login section')
 })
 
 console.log(`Listening to port: ` + `${port}`.red)
+
+app.listen(port);
